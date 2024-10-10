1948aa56d3a79592f628b3be26174e4b11195204
Abubakar-prime <rafiqabubakar81@gmail.com>
2024-10-09 22:39:57 -0700

    Add files via upload

---
+++ user_account/account/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path('', views.RegisterView.as_view(), name='register'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('home/', views.HomeView.as_view(), name='home'),  
+    path('verify_otp/', views.VerifyOtpView.as_view(), name='verify_otp'),  
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
